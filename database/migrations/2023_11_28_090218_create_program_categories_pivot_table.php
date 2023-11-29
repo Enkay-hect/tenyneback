05312196f0060f00c54f236d0909a593331c64c7
Enkay-hect <fridaynkeneabasi@gmail.com>
2023-11-30 00:07:49 +0100

    new update

--- database/migrations/2023_11_28_090218_create_program_categories_pivot_table.php
+++ database/migrations/2023_11_28_090218_create_program_categories_pivot_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('program_categories_pivot', function (Blueprint $table) {
             $table->id();
-            $table->string('programs_id')->index();
-            $table->string('program_categories_id')->index();
+            $table->foreignId('programs_id')->index();
+            $table->foreignId('program_categories_id')->index();
 
-            $table->timestamps(); 
+            $table->timestamps();
 
             $table->foreign('programs_id')->references('id')->on('programs')->onDelete('cascade');
             $table->foreign('program_categories_id')->references('id')->on('program_categories')->onDelete('cascade');
